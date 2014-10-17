9debeeb74849806f1afea7925bdf7915c19a82f9
Dagmar Timler <dagmar@cell-life.org>
2014-10-17 10:28:59 +0200

    IDART-435: renamed and refactored ReportsController so that we use the ReportsController provided by the celllife-reports framework

--- webapp/src/main/java/org/celllife/appointmentreminders/interfaces/service/CSVReportsController.java
+++ webapp/src/main/java/org/celllife/appointmentreminders/interfaces/service/CSVReportsController.java
@@ -1,7 +1,21 @@
 package org.celllife.appointmentreminders.interfaces.service;
 
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.celllife.appointmentreminders.application.utils.FindMessageStatusesStoredProc;
+import org.celllife.pconfig.model.DateParameter;
 import org.celllife.pconfig.model.Pconfig;
+import org.celllife.pconfig.model.StringParameter;
 import org.celllife.reporting.service.PconfigParameterHtmlService;
 import org.celllife.reporting.service.ReportService;
 import org.celllife.reporting.service.impl.PconfigParameterHtmlServiceImpl;
@@ -9,72 +23,52 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.supercsv.cellprocessor.Optional;
 import org.supercsv.cellprocessor.ift.CellProcessor;
-import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.io.CsvMapWriter;
-import org.supercsv.io.ICsvBeanWriter;
 import org.supercsv.io.ICsvMapWriter;
 import org.supercsv.prefs.CsvPreference;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.util.*;
-
 @Controller
-public class ReportsController {
-
-    @Autowired
-    private ReportService reportService;
+public class CSVReportsController {
 
     @Autowired
     FindMessageStatusesStoredProc findMessageStatusesStoredProc;
+    
+    @Autowired
+    private ReportService reportService;
 
     private PconfigParameterHtmlService pconfigParameterHtmlService = new PconfigParameterHtmlServiceImpl();
 
-    private static Logger log = LoggerFactory.getLogger(ReportsController.class);
+    private static Logger log = LoggerFactory.getLogger(CSVReportsController.class);
 
-    @ResponseBody
-    @RequestMapping(
-            value = "/service/reports",
-            method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public Collection<Pconfig> getReports() {
-        return reportService.getReports();
-    }
-
-    //TODO: need to find a more sophisticated way to do this, not just hard-coding html.
-    @ResponseBody
-    @RequestMapping(value = "/service/getHtml", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
-    public String getHtmlForReport(@RequestParam("reportId") String reportId) {
-        Pconfig pconfig;
-            try {
-                pconfig = reportService.getReportByName(reportId);
-            } catch (Exception e) {
-                return "No such Report.";
-            }
-            String htmlString = pconfigParameterHtmlService.createHtmlFieldsFromPconfig(pconfig, "submitButton");
-            return htmlString;
-    }
-
+    // FIXME: add this to the reportsController so it can be re-used
     @ResponseBody
     @RequestMapping(value = "/service/downloadMessageDetailReport", method = RequestMethod.GET)
-    public void downloadCSVReport(HttpServletRequest request, HttpServletResponse response, @RequestParam("clinicId") String clinicId) {
+    public void downloadCSVReport(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        
+        String reportId = request.getParameter("reportId");
+        if (reportId.isEmpty()) {
+            throw new RuntimeException("Could not retrieve a report with an empty reportId.");
+        }
+
+        Pconfig pconfig = reportService.getReportByName(reportId);
+        Pconfig returnedPconfig = pconfigParameterHtmlService.createPconfigFromHtmlFormSubmission(
+                request.getParameterNames(), request.getParameterMap(), pconfig);
+        
+        String facilityCode = ((StringParameter)returnedPconfig.getParameter("facility_code")).getValue();
+        Date startDate = ((DateParameter)returnedPconfig.getParameter("start_date")).getValue();
+        Date endDate = ((DateParameter)returnedPconfig.getParameter("end_date")).getValue();
 
         Map<String, Object> parameters = new HashMap<>();
         List<HashMap<String, Object>> rows;
         Integer numberOfRows;
 
-        parameters.put("clinicId", clinicId);
+        parameters.put("clinicId", facilityCode); // FIXME: use facilityCode + start and end dates in stored procedure
         Map<String, Object> results;
         try {
             results = findMessageStatusesStoredProc.execute(parameters);
@@ -105,7 +99,8 @@ public class ReportsController {
 
             } else {
                 response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                log.warn("No data found for message report of clinic. Could not generate report." + clinicId);
+                log.warn("No data found for message report of clinic. Could not generate report for facility with code "
+                        + facilityCode + " between " + startDate + " and " + endDate);
             }
 
         } catch (IOException | NullPointerException e) {
@@ -122,6 +117,4 @@ public class ReportsController {
         }
         return processors;
     }
-
-
 }
